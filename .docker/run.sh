ff1a32fd1b843518e70692f22e98216086a85025
nutshell113 <chuantran117@gmail.com>
2020-11-02 13:20:29 +0700

    ci: change cmd entrypoint (#238)

--- .docker/run.sh
+++ .docker/run.sh
@@ -6,9 +6,8 @@ docker run \
   --name codelab \
   --expose 3000 \
   --publish 3000:3000 \
-  --entrypoint="['yarn', 'start:prod:web']" \
   --env WEB_PORT=3000 \
   --env NEXT_PUBLIC_API_ORIGIN=http://localhost:4000 \
   --env NEXT_PUBLIC_API_PATHNAME=api/v1 \
-  codelabai/codelab:0.01
-
+  codelabai/codelab:0.01 \
+  yarn start:prod:web
