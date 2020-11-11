7e03001bde9968e566ce69586a8cd0204106f06a
nutshell113 <chuantran117@gmail.com>
2020-11-11 11:09:58 +0700

    ci: increase number of node on prod cluster is 2 (#279)

--- .terraform/cluster.tf
+++ .terraform/cluster.tf
@@ -7,6 +7,6 @@ resource "digitalocean_kubernetes_cluster" "prod_codelab" {
   node_pool {
     name       = "worker-pool"
     size       = "s-2vcpu-2gb"
-    node_count = 1
+    node_count = 2
   }
 }
