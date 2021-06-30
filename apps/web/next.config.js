3a7723243d0f39e5c168280322ed8d1b70d3aea0
svitalii33 <79441726+svitalii33@users.noreply.github.com>
2021-06-30 06:10:08 +0300

    fix: compile css on production build (#905)

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,48 +1,31 @@
 const withNx = require('@nrwl/next/plugins/with-nx')
-// const withCSS = require('@zeit/next-css')
-// const withLess = require('@zeit/next-less')
-// const withSass = require('@zeit/next-sass')
-const withAntdLess = require('next-plugin-antd-less')
-const withPlugins = require('next-compose-plugins')
-const { getThemeVariables } = require('antd/dist/theme')
+const withLess = require('@zeit/next-less')
+const withSass = require('@zeit/next-sass')
+const withCSS = require('@zeit/next-css')
 
-// eslint-disable-next-line padding-line-between-statements
-const isProd = process.env.NODE_ENV === 'production'
-
-// fix: prevents error when .less files are required by node
-// if (typeof require !== 'undefined') {
-//   require.extensions['.less'] = (file) => null;
-// }
-
-// Order matters!
-const nextConfig = withPlugins([
-  [withNx, {}],
-  withAntdLess({
-    modifyVars: {
-      // '@primary-color': '#04f'
-    },
-    lessVarsFilePath: './src/styles/App.less',
-    // lessVarsFilePathAppendToEndOfContent: true,
-    // optional https://github.com/webpack-contrib/css-loader#object
-    cssLoaderOptions: {},
-
-    // Other Config Here...
-
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        // Unset client-side javascript that only works server-side
-        // https://github.com/vercel/next.js/issues/7755#issuecomment-508633125
-        config.node = { fs: 'empty', module: 'empty' }
-      }
-
-      return config
-    },
-
-    // future: {
-    //   // if you use webpack5
-    //   webpack5: true,
-    // },
+module.exports = withLess({
+  ...withNx({
+    cssModules: false,
   }),
-])
+  ...withSass({
+    lessLoaderOptions: {
+      lessOptions: {
+        javascriptEnabled: true,
+      },
+    },
+    ...withCSS({
+      cssLoaderOptions: {
+        url: false,
+      },
+    }),
+  }),
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      // Unset client-side javascript that only works server-side
+      // https://github.com/vercel/next.js/issues/7755#issuecomment-508633125
+      config.node = { fs: 'empty', module: 'empty' }
+    }
 
-module.exports = nextConfig
+    return config
+  },
+})
