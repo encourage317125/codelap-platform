4495141169e67764f996505bba33cab8572003ff
Webber Wang <webber@codelab.app>
2021-07-16 15:50:11 +0800

    ci: update nx command

--- libs/backend/src/common/tree/node.ts
+++ libs/backend/src/common/tree/node.ts
@@ -1,7 +1,7 @@
 import { Entity } from '../entity/entity'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Node extends Entity {
-  children: Array<Node>
-  data: any
+export interface Node<TData> extends Entity {
+  children: Array<Node<TData>>
+  data: TData
 }
