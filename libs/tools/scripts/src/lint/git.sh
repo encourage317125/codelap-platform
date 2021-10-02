1973bfffaf7a0e57738947bc69db0b02d3ff910f
Webber Wang <webber@codelab.app>
2021-10-02 16:04:05 +0800

    ci: add tag to circleci
    ci: add tag to circleci
    
    ci: add tag to circleci
    
    ci: add tag to circleci
    
    ci: add tag to circleci
    
    ci: add tag to circleci
    
    ci: add tag to circleci

--- libs/tools/scripts/src/lint/git.sh
+++ libs/tools/scripts/src/lint/git.sh
@@ -2,10 +2,15 @@
 
 local_branch_name="$(git rev-parse --abbrev-ref HEAD)"
 
-valid_branch_regex='^(([0-9]+)(-[a-z][a-z0-9]*)+$'
+echo $local_branch_name
 
-valid_branch_regex="^([0-9]+)(-[a-z][a-z0-9]*)+$"
-# valid_branch_regex="^.*$"
+# (1) Matches for git feature branch pattern, number followed by lower kebab case
+# e.g. 1023-some-feature
+# (2) Git tag pattern, prod-*.*.* or stag-*.*.*
+# e.g. prod-0.0.1
+# (3) master, for pushing tags
+
+valid_branch_regex="^(([0-9]+)(-[a-z][a-z0-9]*)+)|((stag|prod)-(\d|[1-9]\d*)\.(\d|[1-9]\d*)\.(\d|[1-9]\d*)|master)$"
 
 message="
 Branch names must be lower kebab case starting with the issue number:\n
