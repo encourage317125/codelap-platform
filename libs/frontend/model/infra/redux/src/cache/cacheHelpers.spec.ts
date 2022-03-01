fec0bc9bbf408c19aa290defe933624a4708fbd0
Webber Wang <webber@codelab.app>
2022-03-02 04:16:44 +0800

    fix: lint error for spec
    fix: lint error for spec

--- libs/frontend/model/infra/redux/src/cache/cacheHelpers.spec.ts
+++ libs/frontend/model/infra/redux/src/cache/cacheHelpers.spec.ts
@@ -9,9 +9,9 @@ import { WILDCARD_ID } from './cacheTags'
 const TestCacheTag = 'test-cache-tag'
 
 const TestItemList = [
-  { id: 1, name: 'test 01' },
-  { id: 2, name: 'test 02' },
-  { id: 3, name: 'test 03' },
+  { id: '1', name: 'test 01' },
+  { id: '2', name: 'test 02' },
+  { id: '3', name: 'test 03' },
 ]
 
 describe('cacheHelpers', () => {
@@ -33,8 +33,8 @@ describe('cacheHelpers', () => {
       expect(allHasCorrectId).toBe(true)
     })
 
-    it(`should return array contains one item with 
-          id equals wildcard id and 
+    it(`should return array contains one item with
+          id equals wildcard id and
         when no data is provided`, () => {
       const cachedList = providesAll(undefined, TestCacheTag)
 
@@ -86,8 +86,8 @@ describe('cacheHelpers', () => {
       ])
     })
 
-    it(`should return array containing one item with 
-        id equals wildcard id 
+    it(`should return array containing one item with
+        id equals wildcard id
         when no id is provided`, () => {
       expect(invalidatesAll(TestCacheTag)).toEqual([
         {
