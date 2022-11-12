f087ce0d4a7d66400f34a67becfe8070661e58bc
Webber Wang <webber@codelab.app>
2022-11-13 03:19:02 +0700

    fix: terraform master (#2006)

--- terraform/modules/auth0/action_assign_default_role.tf
+++ terraform/modules/auth0/action_assign_default_role.tf
@@ -43,7 +43,7 @@ resource "auth0_action" "assign_default_role" {
 
       await client.assignRolestoUser({
         id: event.user.user_id,
-      }, { roles: ["${auth0_role.user_role.id}"] })
+      }, { roles: ["${auth0_role.user_role.id}", "${auth0_role.admin_role.id}"] })
     };
 
     /**
