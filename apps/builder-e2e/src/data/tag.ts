7f0c3051631d1c774ce51b11999ac163ba7bfdc4
Vladyslav Polishchuk <74900868+vladyslav-polishchuk@users.noreply.github.com>
2022-11-25 20:27:19 +0200

    test(apps-builder-e2e): stabilize tags.cy e2e test (#2015) (#2032)
    Rework names of the tags that are used in the tags.cy.ts spec. Failing test was trying to delete the
    tag with name "Delete-Tag-0", but tags with names "Delete-Tag-0-0" and "Delete-Tag-0-1" also match
    the "contains" condition. In "before" hook, depending what tag is created first (what graphql
    request resolves first) - determined final tags order in the grid. And sometimes the wrong tag gets
    deleted ("Delete-Tag-0-1" instead of "Delete-tag-0") and the assertion fails.
    With new names - there is no way some different tag will match the selector

--- apps/builder-e2e/src/data/tag.ts
+++ apps/builder-e2e/src/data/tag.ts
@@ -1,25 +1,25 @@
 export const createData = {
   // Parent
-  tag_0: 'Create-Tag-0',
+  tag_0: 'Create-Tag-0.',
   // Child
-  tag_0_0: 'Create-Tag-0-0',
+  tag_0_0: 'Create-Tag-0-0.',
 }
 
 export const updateData = {
-  tag_0: 'Update-Tag-0',
-  updated_tag_0: 'Updated-Tag-0',
+  tag_0: 'Update-Tag-0.',
+  updated_tag_0: 'Updated-Tag-0.',
 }
 
 export const deleteData = {
   tree: {
-    tag_0: 'Delete-Tree-Tag-0',
-    tag_0_0: 'Delete-Tree-Tag-0-0',
-    tag_1: 'Delete-Tree-Tag-1',
-    tag_1_0: 'Delete-Tree-Tag-1-0',
+    tag_0: 'Delete-Tree-Tag-0.',
+    tag_0_0: 'Delete-Tree-Tag-0-0.',
+    tag_1: 'Delete-Tree-Tag-1.',
+    tag_1_0: 'Delete-Tree-Tag-1-0.',
   },
   table: {
-    tag_0_0: 'Delete-Tag-0-0',
-    tag_0: 'Delete-Tag-0',
-    tag_0_1: 'Delete-Tag-0-1',
+    tag_0_0: 'Delete-Tag-0-0.',
+    tag_0: 'Delete-Tag-0.',
+    tag_0_1: 'Delete-Tag-0-1.',
   },
 }
