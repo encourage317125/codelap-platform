34f1dcd041982e3121d401620a12df44c5dbde9a
Webber Wang <webber@codelab.app>
2022-12-05 00:51:30 -0800

    ci: modify init script

--- terraform/environments/prod/symlink.sh
+++ terraform/environments/prod/symlink.sh
@@ -8,4 +8,7 @@ ln -s ../shared/module-auth0.tf ./module-auth0.symlink.tf
 
 ln -s ../shared/vars-auth0.tf ./vars-auth0.symlink.tf
 ln -s ../shared/vars-neo4j.tf ./vars-neo4j.symlink.tf
+ln -s ../shared/vars-mailchimp.tf ./vars-mailchimp.symlink.tf
+ln -s ../shared/vars-hotjar.tf ./vars-hotjar.symlink.tf
+ln -s ../shared/vars-intercom.tf ./vars-intercom.symlink.tf
 ln -s ../shared/vars-vercel-builder.tf ./vars-vercel-builder.symlink.tf
