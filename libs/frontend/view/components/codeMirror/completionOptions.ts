22b3e45c3bef65443f5d331a1d650ab9a73a43cf
Jethro Cabaluna <jethrocabaluna310@gmail.com>
2023-04-14 09:38:06 +0800

    fix: crash when element has react node prop (#2475)

--- libs/frontend/view/components/codeMirror/completionOptions.ts
+++ libs/frontend/view/components/codeMirror/completionOptions.ts
@@ -4,23 +4,33 @@ import capitalize from 'lodash/capitalize'
 import isArray from 'lodash/isArray'
 import isElement from 'lodash/isElement'
 import isObjectLike from 'lodash/isObjectLike'
+import { modelTypeKey } from 'mobx-keystone'
+
+const isReactNode = (obj?: IPropData) => Boolean(obj?.['$$typeof'])
+const isMobxModel = (obj?: IPropData) => Boolean(obj?.[modelTypeKey])
+const isHtmlNode = (obj: unknown) => obj instanceof HTMLElement
+
+const isCyclic = (obj?: IPropData) =>
+  (isObjectLike(obj) && isReactNode(obj)) || isMobxModel(obj) || isHtmlNode(obj)
 
 // for making autocomplete of code mirror
 export const createAutoCompleteOptions = (
   context: IPropData = {},
   parentKey = '',
-): Array<Completion> =>
-  Object.entries(context).flatMap(([key, value]) => {
+): Array<Completion> => {
+  return Object.entries(context).flatMap(([key, value]) => {
     const option = {
       detail: capitalize(typeof value),
       label: parentKey ? `${parentKey}.${key}` : key,
       type: typeof value == 'function' ? 'function' : 'variable',
     }
 
+    if (isCyclic(value)) {
+      return [option]
+    }
+
     if (isArray(value)) {
-      // [item1, item2 ...] = value
       const children = value.flatMap((_value, index) =>
-        // [...autoComplete of item1 [option1,2...], ...autoComplete of item2, ...]
         createAutoCompleteOptions(_value, `${key}.${index}`),
       )
 
@@ -33,3 +43,4 @@ export const createAutoCompleteOptions = (
 
     return [option]
   })
+}
