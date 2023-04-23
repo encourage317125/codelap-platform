6a7d5f9cd2f791a50356cb0a1003e0419c4375d4
Vladyslav Polishchuk <74900868+vladyslav-polishchuk@users.noreply.github.com>
2023-04-23 08:42:32 +0200

    fix: do not replace component root atom with span (#2522)

--- libs/frontend/domain/renderer/src/element/wrapper.utils.ts
+++ libs/frontend/domain/renderer/src/element/wrapper.utils.ts
@@ -19,11 +19,13 @@ export const extractValidProps = (
     : renderOutput.props
 
 export const getReactComponent = (renderOutput: IRenderOutput) => {
+  // if component does not have atom assigned to the root element
   // use span to hold the component's elements together and it is an html
   // element with the least effect on its child and can be used for dnd
-  const atomType = renderOutput.props?.[DATA_COMPONENT_ID]
-    ? IAtomType.HtmlSpan
-    : renderOutput.atomType
+  const atomType =
+    !renderOutput.atomType && renderOutput.props?.[DATA_COMPONENT_ID]
+      ? IAtomType.HtmlSpan
+      : renderOutput.atomType
 
   // Render the atom if it exists, otherwise use fragment
   return atomType ? getAtom(atomType) ?? Fragment : Fragment
