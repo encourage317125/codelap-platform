44d9eb027038ef12c478dcdacd1d334eb35cb98e
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2023-04-25 16:28:25 +0300

    fix: prevent selecting parent component for ReactNodeType prop (#2536)

--- libs/frontend/domain/type/src/interface-form/fields/select-component/SelectComponent.tsx
+++ libs/frontend/domain/type/src/interface-form/fields/select-component/SelectComponent.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import type { IComponent } from '@codelab/frontend/abstract/core'
 import { useStore } from '@codelab/frontend/presenter/container'
 import type { UniformSelectFieldProps } from '@codelab/shared/abstract/types'
 import sortBy from 'lodash/sortBy'
@@ -9,28 +8,27 @@ import { SelectField } from 'uniforms-antd'
 export type SelectComponentProps = Pick<
   UniformSelectFieldProps,
   'error' | 'label' | 'name'
-> & {
-  // Used for filtering the component options
-  parent?: IComponent
-}
+>
 
-export const SelectComponent = ({
-  parent,
-  ...fieldProps
-}: SelectComponentProps) => {
-  const { componentService } = useStore()
+export const SelectComponent = ({ ...fieldProps }: SelectComponentProps) => {
+  const { builderService, componentService } = useStore()
   const allComponents = sortBy(componentService.componentList, 'name')
+  // Used for filtering the component options
+  const parentComponent = builderService.activeComponent?.current
 
   // remove the components that refer the current component to avoid creating circular references
   // including itself
   const filteredComponents = allComponents.filter((component) => {
-    if (component.id === parent?.id) {
+    if (component.id === parentComponent?.id) {
       return false
     }
 
     const descendants = component.descendantComponents
 
-    return !parent?.id || !descendants.some(({ id }) => id === parent.id)
+    return (
+      !parentComponent?.id ||
+      !descendants.some(({ id }) => id === parentComponent.id)
+    )
   })
 
   const componentOptions = filteredComponents.map((component) => ({
